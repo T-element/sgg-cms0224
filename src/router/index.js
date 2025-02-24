8e6c22e8f2eeb17c6e7af00fada1f02993efe60b
cx <444658071@qq.com>
2025-02-24 22:14:34 +0800

    初始化项目

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import login from '@/views/login/login.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      component: login,
+    },
+  ],
+})
+
+export default router
